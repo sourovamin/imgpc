0e8c832de156732319162a662d53bb2fe0ccc600
Sourov Amin <sourov.amin@gmail.com>
2020-03-22 15:45:20 +0100

    Method added for bulk resize

--- imgpx.py
+++ imgpx.py
@@ -10,17 +10,20 @@ class ImgPx:
         if not os.path.exists(folder):
             os.makedirs(folder)
 
-    def destinationFile(self, source_img, des_format, des_folder):
+    def destinationFile(self, source_img, des_folder, des_format = None):
         base_name = os.path.basename(source_img)
         filename = os.path.splitext(base_name)[0]
-        file_ext = filename + "." + des_format
+        extention = os.path.splitext(base_name)[1]
+        if des_format is None:
+            file_ext = filename + extention
+        else:
+            file_ext = filename + "." + des_format
         return os.path.join(des_folder, file_ext)
 
     def saveImage(self, source_img, des_format, des_folder):
         try:
-            self.createFolder(des_folder)
             image = Image.open(source_img)
-            new_image = self.destinationFile(source_img, des_format, des_folder)
+            new_image = self.destinationFile(source_img, des_folder, des_format)
             image.save(new_image)
         except Exception:
             pass
@@ -30,7 +33,23 @@ class ImgPx:
         for file in os.listdir(self.path):
             file_path = os.path.join(self.path, file)
             self.saveImage(file_path, des_format, des_folder)
-            #print(file_path)
 
-    def test(self):
-        print(self.path)
+    def resizeImage(self, source_img, width, height, des_folder, aspect_ratio, des_format ):
+        try:
+            self.createFolder(des_folder)
+            size = width, height
+            image = Image.open(source_img)
+            if aspect_ratio:
+                image.thumbnail((width, height), Image.ANTIALIAS)
+            else:
+                image = image.resize(size)
+            new_image =  self.destinationFile(source_img, des_folder, des_format)
+            image.save(new_image)
+        except Exception:
+            pass
+
+    def resize(self, width, height, des_folder = "Resized", aspect_ratio = True, des_format = None):
+        self.createFolder(des_folder)
+        for file in os.listdir(self.path):
+            file_path = os.path.join(self.path, file)
+            self.resizeImage(file_path, width, height, des_folder, aspect_ratio, des_format)
